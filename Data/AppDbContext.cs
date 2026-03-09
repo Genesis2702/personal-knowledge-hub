3ab9863a4cf8299d83a06ae6a22a3a0c561c0823
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-03-09 10:18:48 +0700

    add: postgresql setup, entity framework

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonalKnowledgeHub.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}

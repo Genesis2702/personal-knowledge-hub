bcce26e18fded170efbc51ae44856da156f690d1
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-03-24 22:03:13 +0700

    add: tag entity

---
+++ Entities/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalKnowledgeHub.Entities
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}

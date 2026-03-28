3d6fe026eb0c7a7a7289a85f662a426226073360
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-03-29 00:04:00 +0700

    add: tag's repository with add, get tags from a specific user, update tag's name and delete

---
+++ Repositories/Implementations/TagRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalKnowledgeHub.Data;
+using PersonalKnowledgeHub.Entities;
+using PersonalKnowledgeHub.Repositories.Interfaces;
+
+namespace PersonalKnowledgeHub.Repositories.Implementations
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public TagRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Tag> AddTagAsync(Tag tag)
+        {
+            await _dbContext.Tags.AddAsync(tag);
+            await _dbContext.SaveChangesAsync();
+            return tag;
+        }
+
+        public async Task<List<Tag>> GetTagsAsync(int userId)
+        {
+            return await _dbContext.Tags.Where(tag => tag.UserId == userId).ToListAsync();
+        }
+
+        public async Task UpdateTagAsync(string name, Tag tag)
+        {
+            tag.Name = name;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteTagAsync(Tag tag)
+        {
+            _dbContext.Tags.Remove(tag);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repositories/Interfaces/ITagRepository.cs
@@ -0,0 +1,12 @@
+﻿using PersonalKnowledgeHub.Entities;
+
+namespace PersonalKnowledgeHub.Repositories.Interfaces
+{
+    public interface ITagRepository
+    {
+        public Task<Tag> AddTagAsync(Tag tag);
+        public Task<List<Tag>> GetTagsAsync(int userId);
+        public Task UpdateTagAsync(string name, Tag tag);
+        public Task DeleteTagAsync(Tag tag);
+    }
+}

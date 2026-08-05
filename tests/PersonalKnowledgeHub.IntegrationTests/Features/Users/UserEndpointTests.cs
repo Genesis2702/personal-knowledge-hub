1988767bd6c79944556e797b955fe668322c7eb8
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-08-05 18:26:17 +0700

    add: empty user endpoint tests file remove: redundant OpenApi package from api project

---
+++ tests/PersonalKnowledgeHub.IntegrationTests/Features/Users/UserEndpointTests.cs
@@ -0,0 +1,12 @@
+﻿using PersonalKnowledgeHub.IntegrationTests.Infrastructure;
+
+namespace PersonalKnowledgeHub.IntegrationTests.Features.Users;
+
+public class UserEndpointTests : IntegrationTestBase
+{
+    public UserEndpointTests(IntegrationFixture fixture) : base(fixture)
+    {
+    }
+    
+    
+}

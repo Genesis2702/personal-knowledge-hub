7b5bc5507c272927126e03d50283e24c507cb42c
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-08-10 08:14:47 +0700

    add: data annotation for request dtos to prevent invalid requests

--- src/PersonalKnowledgeHub.Api/DTOs/Requests/MailData.cs
+++ src/PersonalKnowledgeHub.Api/DTOs/Requests/MailData.cs
@@ -1,9 +1,15 @@
-﻿namespace PersonalKnowledgeHub.DTOs.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalKnowledgeHub.DTOs.Requests;
 
 public class MailData
 {
+    [Required]
     public required string EmailToId { get; set; }
+    [Required]
     public required string EmailToName { get; set; }
+    [Required]
     public required string EmailSubject { get; set; }
+    [Required]
     public required string EmailBody { get; set; }
 }
--- src/PersonalKnowledgeHub.Api/DTOs/Requests/ResourceQueryRequestDto.cs
+++ src/PersonalKnowledgeHub.Api/DTOs/Requests/ResourceQueryRequestDto.cs
@@ -5,7 +5,9 @@ namespace PersonalKnowledgeHub.DTOs.Requests
 {
     public class ResourceQueryRequestDto
     {
+        [Range(1, int.MaxValue)]
         public int PageIndex { get; set; } = 1;
+        [Range(1, int.MaxValue)]
         public int PageSize { get; set; } = 10;
         public int? TagId { get; set; }
         public ResourceType? ResourceType { get; set; }

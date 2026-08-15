782d508757d0d1764dfe2c2f8f062cd5e250199a
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-08-15 14:58:54 +0700

    fix: use execution strategy for transaction instead of explicit transaction due to retry on failure registration for postgres db which caused refresh endpoint to fail refactor: execution strategy transaction in refresh user method

--- src/PersonalKnowledgeHub.Api/Repositories/Implementations/UnitOfWorkRepository.cs
+++ src/PersonalKnowledgeHub.Api/Repositories/Implementations/UnitOfWorkRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
 using PersonalKnowledgeHub.Data;
 using PersonalKnowledgeHub.Repositories.Interfaces;
 
@@ -16,5 +17,27 @@ public class UnitOfWorkRepository : IUnitOfWorkRepository
     public async Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken)
     {
         return await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+    }
+
+    public async Task<T> ExecuteInTransactionAsync<T>(Func<CancellationToken, Task<T>> operation, CancellationToken cancellationToken)
+    {
+        var strategy = _dbContext.Database.CreateExecutionStrategy();
+
+        return await strategy.ExecuteAsync(async strategyCancellationToken =>
+        {
+            await using var transaction = await _dbContext.Database.BeginTransactionAsync(strategyCancellationToken);
+
+            try
+            {
+                T result = await operation(strategyCancellationToken);
+                await transaction.CommitAsync(strategyCancellationToken);
+                return result;
+            }
+            catch
+            {
+                await transaction.RollbackAsync(strategyCancellationToken);
+                throw;
+            }
+        }, cancellationToken);
     }
 }

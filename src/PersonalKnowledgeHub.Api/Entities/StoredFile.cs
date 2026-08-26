e1a76cfa6a279aa66b23715cff2f4d6852dc2fdc
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-08-26 23:06:29 +0700

    add: stored file entity represents the file

---
+++ src/PersonalKnowledgeHub.Api/Entities/StoredFile.cs
@@ -0,0 +1,19 @@
+﻿namespace PersonalKnowledgeHub.Entities
+{
+    public class StoredFile
+    {
+        public int Id { get; set; }
+        public required string StoredKey { get; set; }
+        public required long SizeInBytes { get; set; }
+        public required int ResourceId { get; set; }
+        public Resource? Resource { get; set; }
+        public required FileFormat FileFormat { get; set; }
+    }
+
+    public enum FileFormat
+    {
+        Pdf,
+        Png,
+        Mp4
+    }
+}

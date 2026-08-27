efe62670a5e59af9baa181babbfec8feb0ab7636
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-08-27 23:01:43 +0700

    add: file's storage directory and file storage interface implementation

---
+++ src/PersonalKnowledgeHub.Api/Storage/Interfaces/IFileStorage.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonalKnowledgeHub.Storage.Interfaces;
+
+public interface IFileStorage
+{
+    public Task<string> SaveFile(Stream fileStream, string fileName, CancellationToken cancellationToken);
+    public Task<Stream> OpenFile(string storedKey, CancellationToken cancellationToken);
+    public Task DeleteFile(string storedKey, CancellationToken cancellationToken);
+}

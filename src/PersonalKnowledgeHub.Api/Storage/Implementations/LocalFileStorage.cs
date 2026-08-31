a4eabc9ea1eaa191c2ab0f1b5fcf8968031713f3
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-08-31 18:15:54 +0700

    add: userId to file storage functions to check ownership, stored key validation and resolved path validation functions in local file storage

--- src/PersonalKnowledgeHub.Api/Storage/Implementations/LocalFileStorage.cs
+++ src/PersonalKnowledgeHub.Api/Storage/Implementations/LocalFileStorage.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
+using PersonalKnowledgeHub.Entities;
 using PersonalKnowledgeHub.Storage.Interfaces;
 using PersonalKnowledgeHub.Storage.Options;
 
@@ -9,10 +10,12 @@ public class LocalFileStorage : IFileStorage
     private readonly LocalFileStorageOptions _options;
     private string _targetFolder;
 
-    public LocalFileStorage(IOptions<LocalFileStorageOptions> options)
+    public LocalFileStorage(IOptions<LocalFileStorageOptions> options, IWebHostEnvironment env)
     {
         _options = options.Value;
-        _targetFolder = _options.StorageDirectory;
+        _targetFolder = Path.Combine(env.ContentRootPath, _options.StorageDirectory);
+        _targetFolder = Path.GetFullPath(_targetFolder);
+        Directory.CreateDirectory(_targetFolder);
     }
     
     public async Task<string> SaveFile(Stream fileStream, string fileName, int userId, CancellationToken cancellationToken)
@@ -39,27 +42,110 @@ public class LocalFileStorage : IFileStorage
         return storedKey;
     }
 
-    public async Task<FileStream> OpenFile(string storedKey, CancellationToken cancellationToken)
+    public async Task<Stream> OpenFile(string storedKey, int userId, CancellationToken cancellationToken)
     {
+        if (!IsStoredKeyValid(storedKey, userId))
+        {
+            throw new ArgumentException("The requested path is invalid");
+        }
+            
         string physicalPath = Path.Combine(_targetFolder, storedKey);
+        string normalizedPath = Path.GetFullPath(physicalPath);
 
-        if (!File.Exists(physicalPath))
+        if (!IsFullPathValid(normalizedPath))
+        {
+            throw new ArgumentException("The requested path is invalid");
+        }
+        
+        if (!File.Exists(normalizedPath))
         {
             throw new FileNotFoundException("The requested file does not exist");
         }
         
-        return new FileStream(physicalPath, FileMode.Open, FileAccess.Read);
+        return new FileStream(normalizedPath, FileMode.Open, FileAccess.Read);
     }
 
-    public async Task DeleteFile(string storedKey, CancellationToken cancellationToken)
+    public async Task DeleteFile(string storedKey, int userId, CancellationToken cancellationToken)
     {
+        if (!IsStoredKeyValid(storedKey, userId))
+        {
+            throw new ArgumentException("The requested path is invalid");
+        }
+        
         string physicalPath = Path.Combine(_targetFolder, storedKey);
+        string normalizedPath = Path.GetFullPath(physicalPath);
 
-        if (!File.Exists(physicalPath))
+        if (!IsFullPathValid(normalizedPath))
+        {
+            throw new ArgumentException("The requested path is invalid");
+        }
+
+        if (!File.Exists(normalizedPath))
         {
             throw new FileNotFoundException("The requested file does not exist");
         }
 
-        File.Delete(physicalPath);
+        File.Delete(normalizedPath);
+    }
+
+    private bool IsStoredKeyValid(string storedKey, int userId)
+    {
+        if (String.IsNullOrEmpty(storedKey)) return false;
+        
+        char[] separators =
+        {
+            Path.DirectorySeparatorChar,
+            Path.AltDirectorySeparatorChar
+        };
+
+        string[] segments = storedKey.Split(separators);
+
+        if (segments.Length != 4) return false;
+        
+        string userIdSegment = segments[0];
+        string yearSegment = segments[1];
+        string monthSegment = segments[2];
+        
+        string[] fileSegments = segments[3].Split('.');
+
+        if (fileSegments.Length != 2) return false;
+
+        string fileName = fileSegments[0];
+        string fileExtension = fileSegments[1];
+
+        if (Int32.TryParse(userIdSegment, out int id))
+        {
+            if (id != userId) return false;
+        }
+        else return false;
+
+        if (Int32.TryParse(yearSegment, out int year))
+        {
+            if (year > DateTime.UtcNow.Year) return false;
+        }
+        else return false;
+
+        if (Int32.TryParse(monthSegment, out int month))
+        {
+            if (year < DateTime.UtcNow.Year && (month < 1 || month > 12)) return false;
+            if (year == DateTime.UtcNow.Year && (month > DateTime.UtcNow.Month || month < 1)) return false;
+        }
+        else return false;
+        
+        if (!Guid.TryParse(fileName, out Guid guid)) return false;
+
+        fileExtension = fileExtension.ToLower();
+        if (fileExtension != FileFormat.Pdf.ToString("G").ToLower() && fileExtension != FileFormat.Png.ToString("G").ToLower() && fileExtension != FileFormat.Mp4.ToString("G").ToLower()) return false;
+
+        return true;
+    }
+
+    private bool IsFullPathValid(string normalizedPath)
+    {
+        StringComparison comparison = OperatingSystem.IsWindows() ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
+        string explicitTargetFolder = _targetFolder.EndsWith(Path.DirectorySeparatorChar)
+            ? _targetFolder
+            : _targetFolder + Path.DirectorySeparatorChar;
+        return normalizedPath.StartsWith(explicitTargetFolder, comparison);
     }
 }
--- src/PersonalKnowledgeHub.Api/Storage/Interfaces/IFileStorage.cs
+++ src/PersonalKnowledgeHub.Api/Storage/Interfaces/IFileStorage.cs
@@ -3,6 +3,6 @@
 public interface IFileStorage
 {
     public Task<string> SaveFile(Stream fileStream, string fileName, int userId, CancellationToken cancellationToken);
-    public Task<FileStream> OpenFile(string storedKey, CancellationToken cancellationToken);
-    public Task DeleteFile(string storedKey, CancellationToken cancellationToken);
+    public Task<Stream> OpenFile(string storedKey, int userId, CancellationToken cancellationToken);
+    public Task DeleteFile(string storedKey, int userId, CancellationToken cancellationToken);
 }

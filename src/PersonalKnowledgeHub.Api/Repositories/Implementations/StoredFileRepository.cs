45317e64a14021abc0ae2d67a6b32e7f6062c672
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-09-04 13:11:56 +0700

    add: stored file repository implementation

---
+++ src/PersonalKnowledgeHub.Api/Repositories/Implementations/StoredFileRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalKnowledgeHub.Data;
+using PersonalKnowledgeHub.Entities;
+using PersonalKnowledgeHub.Repositories.Interfaces;
+
+namespace PersonalKnowledgeHub.Repositories.Implementations;
+
+public class StoredFileRepository : IStoredFileRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public StoredFileRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<StoredFile?> GetStoredFileByResourceId(int resourceId)
+    {
+        return await _dbContext.StoredFiles.AsNoTracking().SingleOrDefaultAsync(f => f.ResourceId == resourceId);
+    }
+
+    public async Task<StoredFile?> GetStoredFileByStoredKey(string storedKey)
+    {
+        return await _dbContext.StoredFiles.AsNoTracking().SingleOrDefaultAsync(f => f.StoredKey == storedKey);
+    }
+
+    public async Task<StoredFile?> GetStoredFileById(int id)
+    {
+        return await _dbContext.StoredFiles.AsNoTracking().SingleOrDefaultAsync(f => f.Id == id); 
+    }
+
+    public async Task<StoredFile> AddStoredFile(StoredFile storedFile)
+    {
+        await _dbContext.StoredFiles.AddAsync(storedFile);
+        await _dbContext.SaveChangesAsync();
+        return storedFile;
+    }
+
+    public async Task DeleteStoredFileByStoredKey(string storedKey)
+    {
+        await _dbContext.StoredFiles.Where(f => f.StoredKey == storedKey).ExecuteDeleteAsync();
+        await _dbContext.SaveChangesAsync();
+    }
+}
---
+++ src/PersonalKnowledgeHub.Api/Repositories/Interfaces/IStoredFileRepository.cs
@@ -0,0 +1,12 @@
+﻿using PersonalKnowledgeHub.Entities;
+
+namespace PersonalKnowledgeHub.Repositories.Interfaces;
+
+public interface IStoredFileRepository
+{
+    public Task<StoredFile?> GetStoredFileByResourceId(int resourceId);
+    public Task<StoredFile?> GetStoredFileByStoredKey(string storedKey);
+    public Task<StoredFile?> GetStoredFileById(int id);
+    public Task<StoredFile> AddStoredFile(StoredFile storedFile);
+    public Task DeleteStoredFileByStoredKey(string storedKey);
+}

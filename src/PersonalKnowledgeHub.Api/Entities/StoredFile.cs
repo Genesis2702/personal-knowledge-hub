628a976c83020ecb7ac18cf77964bcdae50fe387
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-09-05 09:39:29 +0700

    add: content type attribute to stored file entity

--- src/PersonalKnowledgeHub.Api/Entities/StoredFile.cs
+++ src/PersonalKnowledgeHub.Api/Entities/StoredFile.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public required string StoredKey { get; set; }
         public required long SizeInBytes { get; set; }
+        public required string ContentType { get; set; }
         public required int ResourceId { get; set; }
         public Resource? Resource { get; set; }
         public required FileFormat FileFormat { get; set; }

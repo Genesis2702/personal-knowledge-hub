c4ecf43c0e188cfadf485c0f730021b34d0e7406
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-09-05 19:20:04 +0700

    add: stored file service's add stored file function implementation

--- src/PersonalKnowledgeHub.Api/Services/Implementations/StoredFileService.cs
+++ src/PersonalKnowledgeHub.Api/Services/Implementations/StoredFileService.cs
@@ -1,21 +1,27 @@
-﻿using PersonalKnowledgeHub.Entities;
+﻿using Microsoft.Extensions.Options;
+using PersonalKnowledgeHub.Entities;
 using PersonalKnowledgeHub.Exceptions;
 using PersonalKnowledgeHub.Models;
 using PersonalKnowledgeHub.Repositories.Interfaces;
 using PersonalKnowledgeHub.Services.Interfaces;
 using PersonalKnowledgeHub.Storage.Interfaces;
+using PersonalKnowledgeHub.Storage.Options;
 
 namespace PersonalKnowledgeHub.Services.Implementations;
 
 public class StoredFileService : IStoredFileService
 {
     private readonly IStoredFileRepository _storedFileRepository;
+    private readonly IResourceRepository _resourceRepository;
     private readonly IFileStorage _fileStorage;
+    private readonly FileUploadOptions _uploadOptions;
 
-    public StoredFileService(IStoredFileRepository storedFileRepository, IFileStorage fileStorage)
+    public StoredFileService(IStoredFileRepository storedFileRepository, IFileStorage fileStorage, IOptions<FileUploadOptions> uploadOptions, IResourceRepository resourceRepository)
     {
         _storedFileRepository = storedFileRepository;
+        _resourceRepository = resourceRepository;
         _fileStorage = fileStorage;
+        _uploadOptions = uploadOptions.Value;
     }
     
     public async Task<StoredFile> GetStoredFileByResourceId(int resourceId, CancellationToken cancellationToken)
@@ -51,7 +57,40 @@ public class StoredFileService : IStoredFileService
     public async Task<StoredFile> AddStoredFile(IFormFile formFile, int userId, int resourceId,
         CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (formFile.Length > _uploadOptions.MaxFileSizeInBytes)
+        {
+            throw new FileSizeLimitExceededException("The requested file is too large");
+        }
+
+        Resource? resource = await _resourceRepository.GetResourceByIdAsync(resourceId, cancellationToken);
+
+        if (resource == null)
+        {
+            throw new NotFoundException("Resource not found");
+        }
+
+        if (resource.UserId != userId)
+        {
+            throw new ForbiddenException("You are not authorized to use this resource");
+        }
+        
+        await using var fileStream = formFile.OpenReadStream();
+        string fileName = formFile.FileName;
+
+        FileResult result = await _fileStorage.SaveFile(fileStream, fileName, userId, cancellationToken);
+
+        StoredFile storedFile = new StoredFile
+        {
+            StoredKey = result.StoredKey,
+            SizeInBytes = result.SizeInBytes,
+            ContentType = result.ContentType,
+            ResourceId = resourceId,
+            FileFormat = result.FileFormat
+        };
+        
+        await _storedFileRepository.AddStoredFile(storedFile, cancellationToken);
+        
+        return storedFile;
     }
 
     public async Task DeleteStoredFileByStoredKey(string storedKey, CancellationToken cancellationToken)

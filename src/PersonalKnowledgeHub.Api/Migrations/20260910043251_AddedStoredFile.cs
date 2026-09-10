b5af0d4fd0bdb130dc53cf01ba5342ba30d632e1
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-09-10 11:43:55 +0700

    add: database migration for stored file configuration

---
+++ src/PersonalKnowledgeHub.Api/Migrations/20260910043251_AddedStoredFile.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PersonalKnowledgeHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedStoredFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StoredFiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    StoredKey = table.Column<string>(type: "text", nullable: false),
+                    SizeInBytes = table.Column<long>(type: "bigint", nullable: false),
+                    ContentType = table.Column<string>(type: "text", nullable: false),
+                    ResourceId = table.Column<int>(type: "integer", nullable: false),
+                    FileFormat = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoredFiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StoredFiles_Resources_ResourceId",
+                        column: x => x.ResourceId,
+                        principalTable: "Resources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoredFiles_ResourceId",
+                table: "StoredFiles",
+                column: "ResourceId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoredFiles_StoredKey",
+                table: "StoredFiles",
+                column: "StoredKey",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StoredFiles");
+        }
+    }
+}

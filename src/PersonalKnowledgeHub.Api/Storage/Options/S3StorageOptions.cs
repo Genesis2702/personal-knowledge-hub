aaee09c1706045e3fad7064ee8ebe59ef7337e69
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-09-19 21:08:59 +0700

    add: s3 storage options

---
+++ src/PersonalKnowledgeHub.Api/Storage/Options/S3StorageOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalKnowledgeHub.Storage.Options;
+
+public sealed class S3StorageOptions
+{
+    public const string Options = "S3StorageOptions";
+    
+    public string BucketName { get; init; } = String.Empty;
+    public string KeyPrefix { get; init; } = "files";
+}
